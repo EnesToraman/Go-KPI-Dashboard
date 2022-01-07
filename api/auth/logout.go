0f91ad0c0a105e4b2952492aac67cc9c63751eb7
EnesToraman <enstrmn@hotmail.com>
2022-01-07 21:29:56 +0300

    Update on logout and ticket.

--- api/auth/logout.go
+++ api/auth/logout.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/EnesToraman/Go-KPI-Dashboard/config"
 	"github.com/julienschmidt/httprouter"
 )
 
 func Logout(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	config.CORS(w, r)
 	http.SetCookie(w, &http.Cookie{
 		Name:     "token",
 		Value:    "",
--- model/ticket.go
+++ model/ticket.go
@@ -1,5 +1,14 @@
 package model
 
+type Ticket struct {
+	Ticket_id    int    `json:"ticketId"`
+	Passenger_id int    `json:"passengerId"`
+	Flight_id    int    `json:"flightId"`
+	Seat         string `json:"seat"`
+	Class        string `json:"class"`
+	Price        int    `json:"price"`
+}
+
 type TicketDataModel struct {
 	NumberOfPassengers int    `json:"numberOfPassengers"`
 	TotalPrice         int    `json:"totalPrice"`
